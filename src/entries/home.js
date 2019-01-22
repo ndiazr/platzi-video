59954acf7670fad9e3eaf1603e67621a9cc36a7d
Nelson Eduardo Diaz Rojas <ediaz@cafetosoftware.com>
2019-01-22 13:38:57 -0500

    * Normalize data using Normalizr dependencie

--- src/entries/home.js
+++ src/entries/home.js
@@ -6,6 +6,9 @@ import data from '../api.json';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/data';
+import normalizedData from '../schemas/index';
+
+console.log(normalizedData);
 
 const initialState = {
   data: {
